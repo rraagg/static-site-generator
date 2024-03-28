c6f65c451e266159c4c8e6dd2aeae58056962b4a
rraagg <rob.a.gallagher@gmail.com>
2024-03-27 22:38:55 -0700

    added textnode to htmlnode function, make lsp and formatter happier

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,40 +2,42 @@ import unittest
 
 from textnode import (
     TextNode,
-    text_type_text,
-    text_type_bold,
-    text_type_italic,
-    text_type_code,
-    text_type_image,
-    text_type_link,
+    TEXT_TYPE_TEXT,
+    TEXT_TYPE_BOLD,
+    TEXT_TYPE_ITALIC,
+    TEXT_TYPE_CODE,
+    TEXT_TYPE_IMAGE,
+    TEXT_TYPE_LINK,
 )
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", text_type_text)
-        node2 = TextNode("This is a text node", text_type_text)
+        node = TextNode("This is a text node", TEXT_TYPE_TEXT)
+        node2 = TextNode("This is a text node", TEXT_TYPE_TEXT)
         self.assertEqual(node, node2)
 
     def test_eq_false(self):
-        node = TextNode("This is a text node", text_type_text)
-        node2 = TextNode("This is a text node", text_type_bold)
+        node = TextNode("This is a text node", TEXT_TYPE_TEXT)
+        node2 = TextNode("This is a text node", TEXT_TYPE_BOLD)
         self.assertNotEqual(node, node2)
 
     def test_eq_false2(self):
-        node = TextNode("This is a text node", text_type_text)
-        node2 = TextNode("This is a text node2", text_type_text)
+        node = TextNode("This is a text node", TEXT_TYPE_TEXT)
+        node2 = TextNode("This is a text node2", TEXT_TYPE_TEXT)
         self.assertNotEqual(node, node2)
 
     def test_eq_url(self):
-        node = TextNode("This is a text node", text_type_italic, "https://www.boot.dev")
+        node = TextNode("This is a text node",
+                        TEXT_TYPE_ITALIC, "https://www.boot.dev")
         node2 = TextNode(
-            "This is a text node", text_type_italic, "https://www.boot.dev"
+            "This is a text node", TEXT_TYPE_ITALIC, "https://www.boot.dev"
         )
         self.assertEqual(node, node2)
 
     def test_repr(self):
-        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        node = TextNode("This is a text node", TEXT_TYPE_TEXT,
+                        "https://www.boot.dev")
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,18 @@
-text_type_text = "text"
-text_type_bold = "bold"
-text_type_italic = "italic"
-text_type_code = "code"
-text_type_link = "link"
-text_type_image = "image"
+"""Module representing TextNodes"""
+
+from htmlnode import LeafNode
+
+TEXT_TYPE_TEXT = "text"
+TEXT_TYPE_BOLD = "bold"
+TEXT_TYPE_ITALIC = "italic"
+TEXT_TYPE_CODE = "code"
+TEXT_TYPE_LINK = "link"
+TEXT_TYPE_IMAGE = "image"
 
 
 class TextNode:
+    """Class representing TextNode"""
+
     def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
@@ -21,3 +27,19 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text == TEXT_TYPE_TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text == TEXT_TYPE_BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text == TEXT_TYPE_ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text == TEXT_TYPE_CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text == TEXT_TYPE_LINK:
+        return LeafNode("a", text_node.text, text_node.url)
+    if text_node.text == TEXT_TYPE_IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Not a valid text type: {text_node.text}")
