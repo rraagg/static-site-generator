0ddde1844dfd220eb6e0253acc2688bfd099f975
rraagg <rob.a.gallagher@gmail.com>
2025-02-11 10:09:12 -0700

    updates to formatting, handling my_header is None

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,8 +1,6 @@
 import os
 from markdown_to_blocks import markdown_to_blocks
-from block_types import (
-    block_to_block_type,
-    BLOCK_TYPE_HEADING)
+from block_types import block_to_block_type, BLOCK_TYPE_HEADING
 from block_to_html import block_to_html, markdown_to_html_node
 
 
@@ -16,25 +14,28 @@ def extract_title(markdown):
 
 
 def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {
-          dest_path} using {template_path}")
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     md_content = ""
     template_content = ""
-    with open(from_path, "r", encoding="utf-8") as markdown, open(template_path, "r", encoding="utf-8")as html_template:
+    with open(from_path, "r", encoding="utf-8") as markdown, open(
+        template_path, "r", encoding="utf-8"
+    ) as html_template:
         md_content = markdown.read()
         template_content = html_template.read()
         markdown.close()
         html_template.close()
 
     my_header = extract_title(md_content)
+    if my_header is None:
+        my_header = ""
     template_title = template_content.replace("{{ Title }}", my_header)
     markdown_to_html = markdown_to_html_node(md_content)
-    final_template = template_title.replace(
-        "{{ Content }}", markdown_to_html.to_html())
+    final_template = template_title.replace("{{ Content }}", markdown_to_html.to_html())
 
     with open(dest_path, "w", encoding="utf-8") as final_html:
         final_html.write(final_template)
         final_html.close()
+
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
     dir_contents = os.listdir(dir_path_content)
@@ -42,7 +43,11 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         current_md_item = os.path.join(dir_path_content, item)
         if os.path.isfile(current_md_item):
             current_html_item = os.path.join(dest_dir_path, item)
-            generate_page(current_md_item, template_path, current_html_item.replace(".md", ".html"))
+            generate_page(
+                current_md_item,
+                template_path,
+                current_html_item.replace(".md", ".html"),
+            )
         else:
             new_dir = os.path.join(dest_dir_path, item)
             if os.path.isdir(new_dir) is False:
